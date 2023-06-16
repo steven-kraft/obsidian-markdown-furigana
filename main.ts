8666631aba9d0a76ffe5fff6d0b41672ed66c992
darknebula05 <camms205@aol.com>
2023-06-15 20:02:45 -0400

    basic live preview support

--- main.ts
+++ main.ts
@@ -1,12 +1,14 @@
 import { Plugin, MarkdownPostProcessor, MarkdownPostProcessorContext } from 'obsidian'
+import { RangeSetBuilder } from "@codemirror/rangeset"
+import { ViewPlugin, WidgetType, EditorView, ViewUpdate, Decoration, DecorationSet } from '@codemirror/view'
 
 // Regular Expression for {{kanji|kana|kana|...}} format
-const REGEXP = /{((?:[\u2E80-\uA4CF\uFF00-\uFFEF])+)((?:\|[^ -\/{-~:-@\[-`]*)+)}/gm;
+const REGEXP = /{((?:[\u2E80-\uA4CF\uFF00-\uFFEF])+)((?:\\?\|[^ -\/{-~:-@\[-`]*)+)}/gm;
 
 // Main Tags to search for Furigana Syntax
 const TAGS = 'p, h1, h2, h3, h4, h5, h6, ol, ul, table'
 
-const convertFurigana = (element:Text): Node => {
+const convertFurigana = (element: Text): Node => {
   const matches = Array.from(element.textContent.matchAll(REGEXP))
   let lastNode = element
   for (const match of matches) {
@@ -20,7 +22,10 @@ const convertFurigana = (element:Text): Node => {
         rubyNode.appendText(k)
         rubyNode.createEl('rt', { text: furi[i] })
       })
-      const nodeToReplace = lastNode.splitText(lastNode.textContent.indexOf(match[0]))
+      console.log(lastNode.length)
+      let offset = lastNode.textContent.indexOf(match[0])
+      console.log("offset: %s", offset)
+      const nodeToReplace = lastNode.splitText(offset)
       lastNode = nodeToReplace.splitText(match[0].length)
       nodeToReplace.replaceWith(rubyNode)
     }
@@ -29,37 +34,120 @@ const convertFurigana = (element:Text): Node => {
 }
 
 export default class MarkdownFurigana extends Plugin {
-    public postprocessor: MarkdownPostProcessor = (el: HTMLElement, ctx: MarkdownPostProcessorContext) => {
-      const blockToReplace = el.querySelectorAll(TAGS)
-      if (blockToReplace.length === 0) return
+  public postprocessor: MarkdownPostProcessor = (el: HTMLElement, ctx: MarkdownPostProcessorContext) => {
+    const blockToReplace = el.querySelectorAll(TAGS)
+    if (blockToReplace.length === 0) return
 
-      function replace (node:Node) {
-        const childrenToReplace: Text[] = []
-        node.childNodes.forEach(child => {
-          if (child.nodeType === 3) {
-            // Nodes of Type 3 are TextElements
-            childrenToReplace.push(child as Text)
-          } else if (child.hasChildNodes() && child.nodeName !== 'CODE' && child.nodeName !== 'RUBY') {
-            // Ignore content in Code Blocks
-            replace(child)
-          }
-        })
-        childrenToReplace.forEach((child) => {
-          child.replaceWith(convertFurigana(child))
-        })
-      }
-      
-      blockToReplace.forEach(block => {
-        replace(block)
+    function replace(node: Node) {
+      const childrenToReplace: Text[] = []
+      node.childNodes.forEach(child => {
+        if (child.nodeType === 3) {
+          // Nodes of Type 3 are TextElements
+          childrenToReplace.push(child as Text)
+        } else if (child.hasChildNodes() && child.nodeName !== 'CODE' && child.nodeName !== 'RUBY') {
+          // Ignore content in Code Blocks
+          replace(child)
+        }
+      })
+      childrenToReplace.forEach((child) => {
+        child.replaceWith(convertFurigana(child))
       })
     }
 
-    async onload () {
-      console.log('loading Markdown Furigana plugin')
-      this.registerMarkdownPostProcessor(this.postprocessor)
+    blockToReplace.forEach(block => {
+      replace(block)
+    })
+  }
+
+  async onload() {
+    console.log('loading Markdown Furigana plugin')
+    this.registerMarkdownPostProcessor(this.postprocessor)
+    this.registerEditorExtension(viewPlugin)
+  }
+
+  onunload() {
+    console.log('unloading Markdown Furigana plugin')
+  }
+}
+
+class RubyWidget extends WidgetType {
+  constructor(readonly kanji: string[], readonly furi: string[]) {
+    super()
+  }
+
+  toDOM(view: EditorView): HTMLElement {
+    let ruby = document.createElement("ruby")
+    this.kanji.forEach((k, i) => {
+      ruby.appendText(k)
+      ruby.createEl("rt", { text: this.furi[i] })
+    })
+    return ruby
+  }
+}
+
+const viewPlugin = ViewPlugin.fromClass(class {
+  decorations: DecorationSet;
+
+  constructor(view: EditorView) {
+    this.decorations = this.buildDecorations(view);
+  }
+
+  update(update: ViewUpdate) {
+    if (
+      update.docChanged ||
+      update.viewportChanged ||
+      update.selectionSet
+    ) {
+      this.decorations = this.buildDecorations(update.view);
+    }
+  }
+
+  destroy() { }
+
+  buildDecorations(view: EditorView): DecorationSet {
+    let builder = new RangeSetBuilder<Decoration>();
+    let lines: number[] = [];
+    if (view.state.doc.length > 0) {
+      lines = Array.from(
+        { length: view.state.doc.lines },
+        (_, i) => i + 1,
+      );
     }
 
-    onunload () {
-      console.log('unloading Markdown Furigana plugin')
+    const currentSelections = [...view.state.selection.ranges];
+
+    for (let n of lines) {
+      const line = view.state.doc.line(n);
+      const startOfLine = line.from;
+      const endOfLine = line.to;
+
+      let currentLine = false;
+
+      currentSelections.forEach((r) => {
+        if (r.to >= startOfLine && r.from <= endOfLine) {
+          currentLine = true;
+          return;
+        }
+      });
+      let matches = Array.from(line.text.matchAll(REGEXP))
+      for (const match of matches) {
+        let add = true
+        const furi = match[2].split("|").slice(1)
+        const kanji = furi.length === 1 ? [match[1]] : match[1].split("")
+        const from = match.index != undefined ? match.index + line.from : -1
+        const to = from + match[0].length
+        currentSelections.forEach((r) => {
+          if (r.to >= from && r.from <= to) {
+            add = false
+          }
+        })
+        if (add) {
+          builder.add(from, to, Decoration.widget({ widget: new RubyWidget(kanji, furi) }))
+        }
+      }
     }
-}
+    return builder.finish();
+  }
+}, {
+  decorations: (v) => v.decorations,
+})
